e65073e5c277920fbd43f10ce25498230c51519a
Kauserali Hafizji <ali.hafizji@gmail.com>
2012-09-06 12:37:13 +0530

    Renaming all files and directories.

---
+++ AppYoda-iOS-SDK/Classes/APQuery.m
@@ -0,0 +1,13 @@
+//
+//  AYQuery.m
+//  AppYoda-iOS-SDK
+//
+//  Created by Kauserali Hafizji on 05/09/12.
+//  Copyright (c) 2012 Appacitive Software Pvt. Ltd. All rights reserved.
+//
+
+#import "AYQuery.h"
+
+@implementation AYQuery
+
+@end
