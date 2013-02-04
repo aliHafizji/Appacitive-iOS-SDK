22c9eeabf3b73919021f1c8acd9a57851d93a439
sandeepdhull1990 <sdhull@tavisca.com>
2013-02-04 20:12:31 +0530

    Fixed the issues in the authentication with facebook and twitter.

--- Appacitive-iOS-SDK/Classes/APUser.m
+++ Appacitive-iOS-SDK/Classes/APUser.m
@@ -91,7 +91,7 @@ static APUser* currentUser = nil;
         NSString *path = [USER_PATH stringByAppendingString:@"authenticate"];
         
         MKNetworkOperation *op = [sharedObject operationWithPath:path
-                                               params:@{@"createNew":@YES, @"type":@"facebook", @"accesstoken":accessToken}.mutableCopy
+                                               params:@{@"createnew":@YES, @"type":@"facebook", @"accesstoken":accessToken}.mutableCopy
                                                httpMethod:@"POST" ssl:YES];
         op.postDataEncoding = MKNKPostDataEncodingTypeJSON;
         [APHelperMethods addHeadersToMKNetworkOperation:op];
@@ -147,7 +147,7 @@ static APUser* currentUser = nil;
         
         MKNetworkOperation *op = [sharedObject
                                   operationWithPath:path
-                                  params:@{@"createNew":@YES, @"type":@"twitter", @"oauthtoken":oauthToken, @"oauthtokensecret":oauthSecret}.mutableCopy
+                                  params:@{@"createnew":@YES, @"type":@"twitter", @"oauthtoken":oauthToken, @"oauthtokensecret":oauthSecret}.mutableCopy
                                   httpMethod:@"POST" ssl:YES];
         op.postDataEncoding = MKNKPostDataEncodingTypeJSON;
         
@@ -203,7 +203,7 @@ static APUser* currentUser = nil;
         
         MKNetworkOperation *op = [sharedObject
                                   operationWithPath:path
-                                  params:@{@"createNew":@YES, @"type":@"twitter", @"oauthtoken":oauthToken, @"oauthtokensecret":oauthSecret,
+                                  params:@{@"createnew":@YES, @"type":@"twitter", @"oauthtoken":oauthToken, @"oauthtokensecret":oauthSecret,
                                             @"consumerKey":consumerKey, @"consumerSecret":consumerSecret}.mutableCopy
                                   httpMethod:@"POST" ssl:YES];
         op.postDataEncoding = MKNKPostDataEncodingTypeJSON;
